5f9b53b6b680e661ce8bba3718530ba37f26a0c4
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-04-01 21:20:08 +0200

    add projects

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -133,168 +133,65 @@ export const RESUME_DATA = {
     "Serverless Infrastructure",
     "Sanity",
     "Storyblok",
-    "Performance",
+    "Performance optimization",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Portal for Developers",
       techStack: [
-        "Side Project",
-        "TypeScript",
         "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Blitz.js",
+        "PostrgeSQL",
+        "Prisma",
+        "Serverless Infrastructure",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "Platform to manage content and events for developers.",
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "portal.gitnation.org",
+        href: "https://portal.gitnation.org/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
+      title: "Liga Tennisa",
       techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Next.js",
+        "PWA",
+        "PostrgeSQL",
+        "Prisma",
+        "Serverless Infrastructure",
       ],
       description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+        "A platform to manage tennis league. atptour.com analog for local community.",
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
+        label: "ligatennisa.com",
+        href: "https://ligatennisa.com/",
       },
     },
     {
-      title: "Evercast",
+      title: "Pyroscope",
+      techStack: ["React", "Redux", "Charts", "Flamegraphs", "Profiling"],
+      description: "Cloud native continious profiling platform.",
+      link: {
+        label: "pyroscope.io",
+        href: "https://pyroscope.io/",
+      },
+    },
+    {
+      title: "Online Banking Platform",
       techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
         "React",
-        "Node.js",
-        "GraphQL",
+        "Redux",
+        "Next.js",
+        "Tracking",
+        "Interactivity",
+        "Micro-Frontend",
       ],
       description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+        "Online-banking, credits, mortrage, assets management, map and employee portals.",
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
+        label: "open bank",
+        href: "/",
       },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
